50cf43e1e24d6c5f6aa3fc9efadd53f1994518ce
Huy <huy3001@gmail.com>
2018-02-10 20:08:45 +0700

    Style responsive

--- js/main.js
+++ js/main.js
@@ -10,30 +10,76 @@
     /* Main functions */
     var mainJs = {
         toggleSidebar: function () {
-            var sidebar = $('.sidebar'),
-                headerButton = $('.header__button');
+            var body = $('body'),
+                sidebar = $('.sidebar'),
+                sidebarButton = $('.sidebar_close'),
+                headerButton = $('.header__button'),
+                targetItems = $('.header__button, .sidebar, .sidebar_close');
 
             if (wdWidth < desktop) {
                 if (headerButton.length) {
                     headerButton.on('click', function () {
-                        if (sidebar.hasCalss('shown')) {
+                        if (sidebar.hasClass('shown')) {
                             sidebar.removeClass('shown');
+                            body.removeClass('sidebar_visible');
                         }
                         else {
                             sidebar.addClass('shown');
+                            body.addClass('sidebar_visible');
                         }
                     });
                 }
+
+                if (sidebarButton.length) {
+                    sidebarButton.on('click', function () {
+                        sidebar.removeClass('shown');
+                        body.removeClass('sidebar_visible');
+                    });
+                }
+
+                $(document).on('click', function(e) {
+                    var target = e.target;
+                    if (!$(target).is(targetItems) && !$(target).parents().is(targetItems)) {
+                        sidebar.removeClass('shown');
+                        body.removeClass('sidebar_visible');
+                    }
+                });
             }
             else {
                 sidebar.removeClass('shown');
+                body.removeClass('sidebar_visible');
+            }
+        },
+
+        tabList: function () {
+            var tabItem = $('.tab-item'),
+                tabDetail = $('.tab-detail');
+            if (tabItem.length) {
+                tabItem.each(function () {
+                    var tabLink = $(this).find('.tab-link');
+                    tabLink.on('click', function (e) {
+                        e.preventDefault();
+                        tabItem.removeClass('tab-list__item_active');
+                        $(this).parent().addClass('tab-list__item_active');
+
+                        var activeItem = $(this).attr('href');
+                        setTimeout(function () {
+                            tabDetail.removeClass('tab-detail_active');
+                            $(activeItem).addClass('tab-detail_active');
+                        }, 100);
+                    });
+                });
             }
         }
     };
 
     /* Window ready functions */
     $(window).ready(function () {
+        // Sidebar toggle //
+        mainJs.toggleSidebar();
 
+        // Tab list active //
+        mainJs.tabList();
     });
 
     /* Window resize functions */
