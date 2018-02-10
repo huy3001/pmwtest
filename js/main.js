ce18818a2a6d0a28cddec3582d1579df4c9b975c
Huy <huy3001@gmail.com>
2018-02-10 20:23:22 +0700

    Style mobile

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@
 (function ($) {
     /* Global variables */
     var desktop = 1200,
+        mobile = 768,
         wdWidth = $(window).width();
 
     /* Main functions */
@@ -70,6 +71,21 @@
                     });
                 });
             }
+        },
+
+        codeContent: function () {
+            var infoDetail = $('.info__detail'),
+                infoTable = $('.info__table').parent(),
+                codeBoxPrepend = $('.code-box__prepend'),
+                codeBoxAppend = $('.code-box__append');
+            if (wdWidth < mobile) {
+                codeBoxPrepend.clone().appendTo(infoDetail);
+                codeBoxAppend.clone().appendTo(infoTable);
+            }
+            else {
+                infoDetail.find('.code-box__prepend').remove();
+                infoTable.find('.code-box__append').remove();
+            }
         }
     };
 
@@ -80,6 +96,9 @@
 
         // Tab list active //
         mainJs.tabList();
+
+        // Code content update //
+        mainJs.codeContent();
     });
 
     /* Window resize functions */
@@ -96,6 +115,9 @@
                     // Done resize width ...
                     // Sidebar toggle //
                     mainJs.toggleSidebar();
+
+                    // Code content update //
+                    mainJs.codeContent();
                 }
             }
         }, 100);
